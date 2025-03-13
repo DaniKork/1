004e4f87129bc7dc2137ae55fed3f361bf4c6503
DaniKork <korkinsdanils@gmail.com>
2025-03-13 16:05:14 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let images = ["airbaltic1.jfif", "airbaltic2.jpg", "airbaltic3.jpg"];
-let currentIndex = 1;
-
-function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    document.getElementById("plane").src = images[currentIndex];
-}
